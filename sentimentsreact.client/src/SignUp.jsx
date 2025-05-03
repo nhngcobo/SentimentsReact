b43bc67bfa5dbf1ca4525dd64d6e01b7df90f4a5
Nhlanhla <nfortunengcobo@gmail.com>
2025-05-03 07:50:22 +0200

    Added confirm password functionality

--- sentimentsreact.client/src/SignUp.jsx
+++ sentimentsreact.client/src/SignUp.jsx
@@ -1,63 +1,94 @@
-import * as React from 'react';
-
+import React, { useState } from 'react';
 import { useTheme } from '@mui/material/styles';
-
 import Button from '@mui/material/Button';
-
 import TextField from "@mui/material/TextField";
-
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import Container from "@mui/material//Container";
-
-const providers = [{ id: 'credentials', name: 'Email and Password' }];
-
+import Container from "@mui/material/Container";
 
 export default function CredentialsSignInPage() {
-  const theme = useTheme();
+  const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState('');
+
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [passwordMatchError, setPasswordMatchError] = useState('');
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+    validatePasswords(e.target.value, confirmPassword);
+  };
+
+  const handleConfirmPasswordChange = (e) => {
+    setConfirmPassword(e.target.value);
+    validatePasswords(password, e.target.value);
+  };
+
+  const validatePasswords = (passwordValue, confirmPasswordValue) => {
+    if (confirmPasswordValue && passwordValue !== confirmPasswordValue) {
+      setPasswordMatchError("Passwords do not match");
+    } else {
+      setPasswordMatchError("");
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+      setEmailError('Please enter a valid email address.');
+      return;
+    }
+
+    setEmailError('');
+    console.log("Form submitted with email:", email);
+    // You can proceed to submit form data here
+  };
+
   return (
     <Container component="main" maxWidth="xs">
-      <div >
-        <Typography component="h1" variant="h5" sx={{textAlign: "center", marginTop: "1.5rem"}}>
+      <div>
+        <Typography component="h1" variant="h5" sx={{ textAlign: "center", marginTop: "1.5rem" }}>
           Sign up
         </Typography>
-        <p style={{textAlign: "center", paddingLeft: "1rem"}}>Create new account with your email</p>
-        <form  noValidate>
-          <Grid container spacing={2} >
-            <div style={{display: "flex"}}>
+        <p style={{ textAlign: "center", paddingLeft: "1rem" }}>Create new account with your email</p>
+        <form noValidate onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            <div style={{ display: "flex" }}>
               <TextField
                 autoComplete="fname"
                 name="firstName"
                 variant="outlined"
                 required
                 fullWidth
-                className="firstName"
                 label="First Name"
                 autoFocus
                 sx={{ marginRight: "1rem" }}
               />
               <TextField
                 variant="outlined"
-                required
                 fullWidth
-                className="lastName"
                 label="Last Name"
                 name="lastName"
                 autoComplete="lname"
               />
             </div>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-              />
-               <div style={{display: "flex"}}>
+            <TextField
+              fullWidth
+              required
+              label="Email"
+              type="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              error={Boolean(emailError)}
+              helperText={emailError}
+              sx={{ mt: 2 }}
+            />
+            <div style={{ display: "flex", marginTop: "1rem" }}>
               <TextField
                 variant="outlined"
                 required
@@ -65,21 +96,23 @@ export default function CredentialsSignInPage() {
                 name="password"
                 label="Password"
                 type="password"
-                id="password"
                 autoComplete="current-password"
                 sx={{ marginRight: "1rem" }}
+                onChange={handlePasswordChange}
               />
-                 <TextField
+              <TextField
                 variant="outlined"
                 required
                 fullWidth
-                name="password"
+                name="confirmPassword"
                 label="Confirm Password"
                 type="password"
-                id="password"
                 autoComplete="current-password"
+                onChange={handleConfirmPasswordChange} 
+                error={Boolean(passwordMatchError)}
+                helperText={passwordMatchError}
               />
-              </div>
+            </div>
           </Grid>
           <Button
             type="submit"
@@ -87,10 +120,10 @@ export default function CredentialsSignInPage() {
             variant="contained"
             color="primary"
             sx={{ marginTop: "1rem" }}
-            >
+          >
             Sign Up
           </Button>
-          <Grid container justify="flex-end">
+          <Grid container justifyContent="flex-end" sx={{ mt: 1 }}>
             <Grid item>
               <Link href="/" variant="body2">
                 Already have an account? Sign in
@@ -99,8 +132,7 @@ export default function CredentialsSignInPage() {
           </Grid>
         </form>
       </div>
-      <Box mt={5}>
-      </Box>
+      <Box mt={5} />
     </Container>
   );
 }
